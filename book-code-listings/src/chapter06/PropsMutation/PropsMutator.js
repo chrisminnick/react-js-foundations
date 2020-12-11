6052a3b730eb3c8582311fabd27e12397f988378
Chris Minnick <chris@minnick.com>
2020-12-10 23:46:34 -0800

    updating 6-1

--- book-code-listings/src/chapter06/PropsMutation/PropsMutator.js
+++ book-code-listings/src/chapter06/PropsMutation/PropsMutator.js
@@ -1,15 +1,17 @@
 function PropsMutator(props){
     let myNumber = props.theNumber;
+
     const changeProp = ()=>{
-        myNumber = myNumber + 1; //don't ever do this
+        myNumber = myNumber + 1;
         console.log("my number is: " + myNumber);
     }
+
     return (
         <>
           <h1>My number is: {myNumber}</h1>
           <h1>props.theNumber is: {props.theNumber}</h1>
-          <button onClick = {changeProp}>mutate props.theNumber</button><br />
-          <button onClick={()=>{props.setTheNumber(props.theNumber + 1)}}>increase the number</button>
+          <button onClick = {changeProp}>change myNumber</button><br />
+          <button onClick={()=>{props.setTheNumber(props.theNumber + 1)}}>use setTheNumber</button>
         </>
     )
 }
