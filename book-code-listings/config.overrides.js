6e4f4a74e18f49aba2275b19c0c4407a75869e4d
Chris Minnick <chris@minnick.com>
2021-10-03 07:35:06 -0700

    fixed redirect to index.html for github pages

---
+++ book-code-listings/config.overrides.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = function override(config, env) {
+
+
+    config.devServer.push(
+        {historyApiFallback: true}
+    );
+    config.output.push(
+        {publicPath: '/'}
+    )
+    return config;
+}
