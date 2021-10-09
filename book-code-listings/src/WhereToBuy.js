ab9c517e3ae5cdc93acc31ba83ebae9e0bd4e6d5
Chris Minnick <chris@minnick.com>
2021-10-09 01:28:18 -0700

    added powells link

--- book-code-listings/src/WhereToBuy.js
+++ book-code-listings/src/WhereToBuy.js
@@ -7,7 +7,7 @@ function WhereToBuy(props){
             more stores to this list as they make the book availble for sale / presale.
         </p>
         <ul>
-            <li><a hreh="https://www.powells.com/book/react-js-foundations-building-user-interfaces-with-reactjs-9781119685548">Powells.com</a>
+            <li><a hreh="https://www.powells.com/book/react-js-foundations-building-user-interfaces-with-reactjs-9781119685548">Powells.com</a></li>
             <li><a href="https://amzn.to/3lbXwya">Amazon.com</a></li>
             <li><a href="https://www.barnesandnoble.com/w/react-js-foundations-building-user-interfaces-with-reactjs-chris-minnick/1140168465?ean=9781119685548">Barnes and Noble</a></li>
             <li><a href="https://www.booksamillion.com/product/9781119685548">Books a Million</a></li>
