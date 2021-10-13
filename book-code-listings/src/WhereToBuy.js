6de3ff925fbcc976ddf84acd129f3f73de28b12e
Chris Minnick <chris@minnick.com>
2021-10-13 02:42:44 -0700

    added link to book on wiley.com

--- book-code-listings/src/WhereToBuy.js
+++ book-code-listings/src/WhereToBuy.js
@@ -7,6 +7,7 @@ function WhereToBuy(props){
             more stores to this list as they make the book availble for sale / presale.
         </p>
         <ul>
+            <li><a href="https://www.wiley.com/en-us/React+JS+Foundations+Building+User+Interfaces+with+ReactJS%3A+An+Approachable+Guide-p-9781119685548">Wiley.com</a></li>
             <li><a href="https://www.powells.com/book/react-js-foundations-building-user-interfaces-with-reactjs-9781119685548">Powells.com</a></li>
             <li><a href="https://amzn.to/3lbXwya">Amazon.com</a></li>
             <li><a href="https://www.barnesandnoble.com/w/react-js-foundations-building-user-interfaces-with-reactjs-chris-minnick/1140168465?ean=9781119685548">Barnes and Noble</a></li>
