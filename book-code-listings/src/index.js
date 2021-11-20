a43f4553ef399ba34ea2d80bc72f860eb2081b4e
Chris Minnick <chris@minnick.com>
2021-11-19 16:51:02 -0800

    fixed google analytics tracking

--- book-code-listings/src/index.js
+++ book-code-listings/src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
