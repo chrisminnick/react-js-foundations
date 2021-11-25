aa5dea4a8cf43647fbb69bde44932e5372724ebd
Chris Minnick <chris@minnick.com>
2021-11-25 06:52:37 -0800

    removing author picture

--- book-code-listings/src/About.js
+++ book-code-listings/src/About.js
@@ -1,29 +1,33 @@
 import "./About.css";
-import Helmet from 'react-helmet';
+import Helmet from "react-helmet";
 
 function About() {
   return (
     <>
-    <Helmet> 
-      <title>React JS Foundations by Chris Minnick - About the Author</title>
-      <meta name="description" content="Get up to speed on building applications with ReactJS with this
+      <Helmet>
+        <title>React JS Foundations by Chris Minnick - About the Author</title>
+        <meta
+          name="description"
+          content="Get up to speed on building applications with ReactJS with this
         practical yet very approachable book that will provide you with
         everything you need to understand what React is and how to start
-        building applications with it." />
-    </Helmet>
+        building applications with it."
+        />
+      </Helmet>
       <h2>About the Author</h2>
       <p>
-        Chris Minnick has been a full stack developer for over 25 years, and a
-        professional author and trainer for 15 years. He has been the lead
-        front-end React developer for several startup companies, and has been
-        building web and mobile user interfaces with React since 2015. Chris has
-        produced online video courses for Pluralsight and O'Reilly Media and is
-        the author of over a dozen books including React JS Foundations:
-        Building User Interfaces with React JS.
+        <a href="https://chrisminnick.com" target="_blank">Chris Minnick</a> has 
+        been a full stack developer for over 25 years, and a professional
+        author and trainer for 15 years. He has been the lead front-end React
+        developer for several startup companies, and has been building web and
+        mobile user interfaces with React since 2015. Chris has produced online
+        video courses for Pluralsight and O'Reilly Media and is the author of
+        over a dozen books including React JS Foundations: Building User
+        Interfaces with React JS.
       </p>
 
       <h3>Other Books by Chris Minnick</h3>
-      <ul style={{listStyleType:"none"}}>
+      <ul style={{ listStyleType: "none" }}>
         <li>
           <a href="https://amzn.to/3EdqbLR">
             Coding with JavaScript for Dummies
