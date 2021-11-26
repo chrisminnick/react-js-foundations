b797f69e02c44504cf011dc4e962fcc27dede1f6
Chris Minnick <chris@minnick.com>
2021-11-26 05:27:17 -0800

    linking book image

--- book-code-listings/src/Introduction.js
+++ book-code-listings/src/Introduction.js
@@ -4,7 +4,9 @@ function Introduction(props) {
   return (
     <>
     <div style={{textAlign:"center"}}>
+      <a href="https://www.amazon.com/React-Foundations-Building-Interfaces-ReactJS/dp/1119685540?keywords=react+js+foundations+minnick&qid=1637085780&qsid=144-7321471-0673327&sr=8-1&sres=1119685540%2CB082KZ8ZGM%2CB00PFCSURS%2CB084W9YBXD%2CB07J9KNS9G%2CB07GX6DSRW%2CB08TY6413H%2CB004675EI6%2CB002LF0J1M%2CB00DQL8HKK%2CB01LXJ0GF3%2CB01DPA6JXS%2CB01L2ZRYVE%2CB074VD4X89%2CB07SJYX8DV%2CB01M0H2R13&srpt=ABIS_BOOK&linkCode=sl1&tag=chrismincom-20&linkId=ad221fcbab9a5e438b88d2a0b3cb44c9&language=en_US&ref_=as_li_ss_tl">
       <img src="/images/cover.jpg" width="395" height="498" style={{maxWidth:"100%",height:"auto"}} alt="React JS Foundations" />
+      </a>
     </div>
     <Helmet> 
       <title>React JS Foundations by Chris Minnick - Building User Interfaces with ReactJS: An
