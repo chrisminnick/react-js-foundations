c4f4b5e9d312f7c817dbfc7f345f2cbce54ffe79
Chris Minnick <chris@minnick.com>
2021-12-30 04:46:30 -0800

    fixing formatting

--- reactjsfoundations.com/src/chapter03/Header.js
+++ reactjsfoundations.com/src/chapter03/Header.js
@@ -2,7 +2,7 @@ function Header(props) {
   return (
     <>
       <h1 style={{ fontSize: "24px", color: "blue" }}>Welcome to My Website</h1>
-      <pre>
+      {/* <pre>
         {`function Header(props){
     return (
       <h1 style={{fontSize:"24px",color:"blue"}}>
@@ -11,7 +11,7 @@ function Header(props) {
       )
     }
 export default Header; `}
-      </pre>
+      </pre> */}
     </>
   );
 }
