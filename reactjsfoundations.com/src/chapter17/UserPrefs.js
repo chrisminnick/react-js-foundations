6f880701024b133a09dd7a2c6db2b2c936c0b988
Chris Minnick <chris@minnick.com>
2022-01-18 04:22:03 -0800

    fixed up a listing

--- reactjsfoundations.com/src/chapter17/UserPrefs.js
+++ reactjsfoundations.com/src/chapter17/UserPrefs.js
@@ -14,6 +14,7 @@ const UserPrefs = (props) => {
 
   return (
     <>
+    <div className="listing-result">
       Your preferred length unit: {unitPrefs.lengthUnit}&nbsp;
       <button onClick={changeLengthUnit}>
         Switch to {unitPrefs.lengthUnit === "cm" ? "inch" : "cm"}
@@ -23,39 +24,48 @@ const UserPrefs = (props) => {
       <button onClick={changeTempUnit}>
         Switch to {unitPrefs.tempUnit === "c" ? "f" : "c"}
       </button>
-      <br />
-      <br />
-      <pre>
-        {`import {useContext} from 'react';
-import {UnitsContext} from './contexts/UnitsContext';
+      </div>
+      <CodeAndText />
 
-const UserPrefs = (props) => {
-
-    const unitPrefs = useContext(UnitsContext);
-
-    const changeLengthUnit = () => {
-        unitPrefs.setLengthUnit((unitPrefs.lengthUnit === 'cm')?"inch":"cm");
-    }
-
-    const changeTempUnit = () => {
-        unitPrefs.setTempUnit((unitPrefs.tempUnit === 'c')?"f":"c");
-    }
-
-    return (
-        <>
-          Your preferred length unit: {unitPrefs.lengthUnit}&nbsp;
-          <button onClick={changeLengthUnit}>Switch to {(unitPrefs.lengthUnit === 'cm')?"inch":"cm"}</button><br />
-          Your preferred temperature unit: {unitPrefs.tempUnit}&nbsp;
-          <button onClick={changeTempUnit}>Switch to {(unitPrefs.tempUnit === 'c')?"f":"c"}</button><br />
-<br />
-        </>
-    )
-}
-
-export default UserPrefs;`}
-      </pre>
+      
     </>
   );
 };
 
+function CodeAndText(){
+  return (
+    <pre>
+        {`import { useContext } from "react";
+import { UnitsContext } from "./contexts/UnitsContext";
+
+const UserPrefs = (props) => {
+  const unitPrefs = useContext(UnitsContext);
+
+  const changeLengthUnit = () => {
+    unitPrefs.setLengthUnit(unitPrefs.lengthUnit === "cm" ? "inch" : "cm");
+  };
+
+  const changeTempUnit = () => {
+    unitPrefs.setTempUnit(unitPrefs.tempUnit === "c" ? "f" : "c");
+  };
+
+  return (
+    <>
+      Your preferred length unit: {unitPrefs.lengthUnit}&nbsp;
+      <button onClick={changeLengthUnit}>
+        Switch to {unitPrefs.lengthUnit === "cm" ? "inch" : "cm"}
+      </button>
+      <br />
+      Your preferred temperature unit: {unitPrefs.tempUnit}&nbsp;
+      <button onClick={changeTempUnit}>
+        Switch to {unitPrefs.tempUnit === "c" ? "f" : "c"}
+      </button>    
+    </>
+  );
+};
+
+export default UserPrefs;`}
+      </pre>
+  )
+}
 export default UserPrefs;
