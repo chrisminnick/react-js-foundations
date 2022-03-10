f1245d2c6e52adcd65d95f27f62b5e05eb9a8578
Chris Minnick <chris@minnick.com>
2022-03-10 11:08:22 -0800

    adding to footer

--- reactjsfoundations.com/src/CodeLinks.js
+++ reactjsfoundations.com/src/CodeLinks.js
@@ -1,18 +1,28 @@
 function CodeLinks() {
   return (
     <>
-      <p style={{backgroundColor:"#ddd",padding:"10px",fontSize:"1em"}}>
-        Download the examples, report issues, and ask/answer questions
-        in the discussion area by visiting the book's{" "}
-        <a href="https://github.com/chrisminnick/react-js-foundations">
-          github page
-        </a>
-        . All of the code for the book is also available on{" "}
-        <a href="https://codesandbox.io/s/react-js-foundations-9mbn3">
-          codesandbox.io
-        </a>{" "}
-        for you to play around with.
-      </p>
+      <div
+        style={{ backgroundColor: '#ddd', padding: '10px', fontSize: '1em' }}
+      >
+        <p>
+          Download the examples, report issues, and ask/answer questions in the
+          discussion area by visiting the book's{' '}
+          <a href="https://github.com/chrisminnick/react-js-foundations">
+            github page
+          </a>
+          . All of the code for the book is also available on{' '}
+          <a href="https://codesandbox.io/s/react-js-foundations-9mbn3">
+            codesandbox.io
+          </a>{' '}
+          for you to play around with.
+        </p>
+        <p>
+          ReactJS Foundations is published John Wiley and Sons, Inc and is
+          available in{' '}
+          <a href="https://www.amazon.com/dp/1119685540">paperback and eBook</a>
+          .
+        </p>
+      </div>
     </>
   );
 }
