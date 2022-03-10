6886599cb51796298fb7b2cbe0ec0c1b31a1b6c3
Chris Minnick <chris@minnick.com>
2022-03-10 02:59:05 -0800

    added buy link to homepage

--- reactjsfoundations.com/src/Introduction.js
+++ reactjsfoundations.com/src/Introduction.js
@@ -3,19 +3,51 @@ import Helmet from 'react-helmet';
 function Introduction(props) {
   return (
     <>
-    <div style={{textAlign:"center"}}>
-      <a href="https://www.amazon.com/React-Foundations-Building-Interfaces-ReactJS/dp/1119685540?keywords=react+js+foundations+minnick&qid=1637085780&qsid=144-7321471-0673327&sr=8-1&sres=1119685540%2CB082KZ8ZGM%2CB00PFCSURS%2CB084W9YBXD%2CB07J9KNS9G%2CB07GX6DSRW%2CB08TY6413H%2CB004675EI6%2CB002LF0J1M%2CB00DQL8HKK%2CB01LXJ0GF3%2CB01DPA6JXS%2CB01L2ZRYVE%2CB074VD4X89%2CB07SJYX8DV%2CB01M0H2R13&srpt=ABIS_BOOK&linkCode=sl1&tag=chrismincom-20&linkId=ad221fcbab9a5e438b88d2a0b3cb44c9&language=en_US&ref_=as_li_ss_tl">
-      <img src="/images/cover.jpg" width="300" height="378" style={{maxWidth:"100%",height:"auto"}} alt="React JS Foundations" />
-      </a>
-    </div>
-    <Helmet> 
-      <title>React JS Foundations - Building User Interfaces with ReactJS: An
-          Approachable Guide</title>
-      <meta name="description" content="Get up to speed on building applications with ReactJS with this
+      <div style={{ textAlign: 'center' }}>
+        <a href="https://www.amazon.com/React-Foundations-Building-Interfaces-ReactJS/dp/1119685540">
+          <img
+            src="/images/cover.jpg"
+            width="300"
+            height="378"
+            style={{ maxWidth: '100%', height: 'auto' }}
+            alt="React JS Foundations"
+          />
+        </a>
+      </div>
+      <div style={{ textAlign: 'center', spadding: '4px 12px 0px 0px' }}>
+        <button
+          style={{
+            height: '40px',
+            padding: '4px 20px',
+            width: '300px',
+            verticalAlign: 'middle',
+            border: '1px solid #666',
+            borderRadius: '2px',
+            borderColor: '#1b1f2426',
+            backgroundColor: '#2da44e',
+          }}
+        >
+          <a
+            style={{ color: '#fff' }}
+            href="https://www.amazon.com/React-Foundations-Building-Interfaces-ReactJS/dp/1119685540"
+          >
+            Preview and Buy Now @ Amazon.com
+          </a>
+        </button>
+      </div>
+      <Helmet>
+        <title>
+          React JS Foundations - Building User Interfaces with ReactJS: An
+          Approachable Guide
+        </title>
+        <meta
+          name="description"
+          content="Get up to speed on building applications with ReactJS with this
         practical yet very approachable book that will provide you with
         everything you need to understand what React is and how to start
-        building applications with it." />
-    </Helmet>
+        building applications with it."
+        />
+      </Helmet>
 
       <p>
         Get up to speed on building applications with ReactJS with this
--- reactjsfoundations.com/src/WhereToBuy.js
+++ reactjsfoundations.com/src/WhereToBuy.js
@@ -3,19 +3,22 @@ import Helmet from 'react-helmet';
 function WhereToBuy(props) {
   return (
     <>
-    <Helmet> 
-      <title>ReactJS Foundations by Chris Minnick - Where to Buy the Book</title>
-      <meta name="description" content="Get up to speed on building applications with ReactJS with this
+      <Helmet>
+        <title>
+          ReactJS Foundations by Chris Minnick - Where to Buy the Book
+        </title>
+        <meta
+          name="description"
+          content="Get up to speed on building applications with ReactJS with this
         practical yet very approachable book that will provide you with
         everything you need to understand what React is and how to start
-        building applications with it." />
-    </Helmet>
+        building applications with it."
+        />
+      </Helmet>
       <h1>Where to Buy the Book</h1>
       <p>
         ReactJS Foundations Building User Interfaces with ReactJS: An
         Approachable Guide can be purchased anywhere computer books are sold.
-        The expected release date for the book is in March 2022. I'll add more
-        stores to this list as they make the book availble for sale / presale.
       </p>
       <ul className="inside-links">
         <li>
@@ -29,7 +32,9 @@ function WhereToBuy(props) {
           </a>
         </li>
         <li>
-          <a href="https://www.amazon.com/React-Foundations-Building-Interfaces-ReactJS/dp/1119685540?keywords=react+js+foundations+minnick&qid=1637085780&qsid=144-7321471-0673327&sr=8-1&sres=1119685540%2CB082KZ8ZGM%2CB00PFCSURS%2CB084W9YBXD%2CB07J9KNS9G%2CB07GX6DSRW%2CB08TY6413H%2CB004675EI6%2CB002LF0J1M%2CB00DQL8HKK%2CB01LXJ0GF3%2CB01DPA6JXS%2CB01L2ZRYVE%2CB074VD4X89%2CB07SJYX8DV%2CB01M0H2R13&srpt=ABIS_BOOK&linkCode=sl1&tag=chrismincom-20&linkId=ad221fcbab9a5e438b88d2a0b3cb44c9&language=en_US&ref_=as_li_ss_tl">Amazon.com</a>
+          <a href="https://www.amazon.com/React-Foundations-Building-Interfaces-ReactJS/dp/1119685540?keywords=react+js+foundations+minnick&qid=1637085780&qsid=144-7321471-0673327&sr=8-1&sres=1119685540%2CB082KZ8ZGM%2CB00PFCSURS%2CB084W9YBXD%2CB07J9KNS9G%2CB07GX6DSRW%2CB08TY6413H%2CB004675EI6%2CB002LF0J1M%2CB00DQL8HKK%2CB01LXJ0GF3%2CB01DPA6JXS%2CB01L2ZRYVE%2CB074VD4X89%2CB07SJYX8DV%2CB01M0H2R13&srpt=ABIS_BOOK&linkCode=sl1&tag=chrismincom-20&linkId=ad221fcbab9a5e438b88d2a0b3cb44c9&language=en_US&ref_=as_li_ss_tl">
+            Amazon.com
+          </a>
         </li>
         <li>
           <a href="https://www.barnesandnoble.com/w/react-js-foundations-building-user-interfaces-with-reactjs-chris-minnick/1140168465?ean=9781119685548">
@@ -47,9 +52,7 @@ function WhereToBuy(props) {
           </a>
         </li>
         <li>
-          <a href="https://www.indiebound.org/book/9781119685548">
-            Indiebound
-          </a>
+          <a href="https://www.indiebound.org/book/9781119685548">Indiebound</a>
         </li>
       </ul>
     </>
