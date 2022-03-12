34d5fb38db9999f3b5bad62a7936af2260a3e825
Chris Minnick <chris@minnick.com>
2022-03-12 05:20:58 -0800

    fixing article

--- reactjsfoundations.com/src/chapter03/ConditionalWelcome2.js
+++ reactjsfoundations.com/src/chapter03/ConditionalWelcome2.js
@@ -33,9 +33,10 @@ export default Welcome;`}
       </SyntaxHighlighter>
       <h1>Conditional Rendering with &amp;&amp;</h1>
       <p>
-        The &amp;&amp; operator relies on the fact that any operand in
-        JavaScript can be converted to a Boolean true or a Boolean false. The
-        following values evaluate to <code>false</code>:
+        The <code>&amp;&amp;</code> operator relies on the fact that any operand
+        in JavaScript can be converted to a Boolean <code>true</code> or a
+        Boolean <code>false</code>. The following values evaluate to{' '}
+        <code>false</code>:
       </p>
       <ul>
         <li>false (Boolean false)</li>
@@ -48,15 +49,30 @@ export default Welcome;`}
         <li>NaN (Not a number)</li>
       </ul>
       <p>
-        Values that evaluate to a Boolean false are said to be "falsy." All
-        other values evaluate to a Boolean true and are known as "truthy."
+        Values that evaluate to a Boolean <code>false</code> are said to be
+        "falsy." All other values evaluate to a Boolean <code>true</code> and
+        are known as "truthy."
       </p>
       <p>
-        If the operand on the left of JavaScript's &amp;&amp; operator evaluates
-        to a Boolean false, the value on the left is returned. If the operand on
-        the left evaluates to a Boolean true, the operand on the right is
-        evaluated. If the operand on the right evaluates to a Boolean true, the
-        value on the right is returned.
+        If the operand on the left of JavaScript's <code>&amp;&amp;</code>{' '}
+        operator evaluates to a Boolean <code>false</code>, the value on the
+        left is returned. If the operand on the left evaluates to a Boolean{' '}
+        <code>true</code>, the operand on the right is evaluated. If the operand
+        on the right evaluates to a Boolean <code>true</code>, the value on the
+        right is returned.
+      </p>
+      <p>
+        In the above example, if the value of <code>loggedIn</code> is{' '}
+        <code>false</code>, the statement will return <code>false</code> and
+        nothing will render. If <code>loggedIn</code> is <code>true</code>, the
+        component on the right will render.
+      </p>
+      <p>
+        Using the <code>&amp;&amp;</code> operator is a quick and easy way to
+        conditionally render a component. Unlike using an <code>if/else</code>{' '}
+        statement, a <code>switch</code> statement, or the ternary operator,
+        using <code>&amp;&amp;</code>
+        doesn't provide a way to choose between 2 or more components to render.
       </p>
     </>
   );
