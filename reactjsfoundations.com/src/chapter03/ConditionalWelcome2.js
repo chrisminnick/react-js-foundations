eba22e9032ffc1532e025464347cfa065d9080b3
Chris Minnick <chris@minnick.com>
2022-03-12 05:09:10 -0800

    new conditional rendering explanation

--- reactjsfoundations.com/src/chapter03/ConditionalWelcome2.js
+++ reactjsfoundations.com/src/chapter03/ConditionalWelcome2.js
@@ -15,8 +15,9 @@ function Welcome({ loggedIn }) {
 
 function CodeAndText() {
   return (
-    <SyntaxHighlighter language="javascript" style={github}>
-      {`import Header from './Header';
+    <>
+      <SyntaxHighlighter language="javascript" style={github}>
+        {`import Header from './Header';
 
 function Welcome({loggedIn}){
     return (
@@ -29,7 +30,35 @@ function Welcome({loggedIn}){
   }
   
 export default Welcome;`}
-    </SyntaxHighlighter>
+      </SyntaxHighlighter>
+      <h1>Conditional Rendering with Element Variables</h1>
+      <p>
+        The &amp;&amp; operator relies on the fact that any operand in
+        JavaScript can be converted to a Boolean true or a Boolean false. The
+        following values evaluate to <code>false</code>:
+      </p>
+      <ul>
+        <li>false (Boolean false)</li>
+        <li>0 (The number 0)</li>
+        <li>-0 (The number -0)</li>
+        <li>0n (The BigInt zero)</li>
+        <li>'' (Empty string)</li>
+        <li>null (The absense of a value)</li>
+        <li>undefined (The undefined data type)</li>
+        <li>NaN (Not a number)</li>
+      </ul>
+      <p>
+        Values that evaluate to a Boolean false are said to be "falsy." All
+        other values evaluate to a Boolean true and are known as "truthy."
+      </p>
+      <p>
+        If the operand on the left of JavaScript's &amp;&amp; operator evaluates
+        to a Boolean false, the value on the left is returned. If the operand on
+        the left evaluates to a Boolean true, the operand on the right is
+        evaluated. If the operand on the right evaluates to a Boolean true, the
+        value on the right is returned.
+      </p>
+    </>
   );
 }
 export default Welcome;
