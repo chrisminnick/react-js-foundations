191e053ddefcba3cc1361121c798dcddec89785f
Chris Minnick <chris@minnick.com>
2022-03-20 13:29:01 -0700

    added social icons

--- reactjsfoundations.com/src/App.js
+++ reactjsfoundations.com/src/App.js
@@ -6,7 +6,7 @@ import CodeLinks from './CodeLinks';
 import BottomNav from './BottomNav';
 import { Suspense } from 'react';
 import { useLocation } from 'react-router-dom';
-
+import { SocialIcon } from 'react-social-icons';
 import WpBlogWidget from './WpBlogWidget';
 
 const MainNav = lazy(() => import('./MainNav'));
@@ -47,6 +47,16 @@ function App(props) {
             <MainNav />
           </Suspense>
           <div id="column3">
+            <div className="social_icons">
+              <SocialIcon
+                url="https://twitter.com/chrisminnick"
+                style={{ height: 40, width: 40, margin: '8px' }}
+              />
+              <SocialIcon
+                url="https://www.linkedin.com/in/chrisminnick/"
+                style={{ height: 40, width: 40, margin: '8px' }}
+              />
+            </div>
             <a
               href="https://www.amazon.com/dp/1119685540?&linkCode=li3&tag=chrismincom-20&linkId=6c69efbcfd2ca6ae74a870e3a88a40db&language=en_US&ref_=as_li_ss_il"
               target="_blank"
@@ -64,6 +74,8 @@ function App(props) {
               alt=""
               style={{ border: 'none !important', margin: '0px !important' }}
             />
+            <br />
+            <br />
             <WpBlogWidget posts="3" title="Latest Blog Posts" />
           </div>
         </Suspense>
