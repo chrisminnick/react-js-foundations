267e217aecf1e0d6720e2002944b849616f7753d
Chris Minnick <chris@minnick.com>
2022-03-27 08:47:10 -0700

    scroll to top

--- reactjsfoundations.com/src/index.js
+++ reactjsfoundations.com/src/index.js
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import ScrollToTop from './ScrollToTop';
+
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
-  document.getElementById("root")
+  <Router>
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
+  </Router>,
+  document.getElementById('root')
 );
