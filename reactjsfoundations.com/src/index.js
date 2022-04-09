c9a38ca856cf85572f2d83d2f7ea785894b9e3ca
Chris Minnick <chris@minnick.com>
2022-04-09 06:25:50 -0700

    upgraded to react 18

--- reactjsfoundations.com/src/index.js
+++ reactjsfoundations.com/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import ScrollToTop from './ScrollToTop';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <ScrollToTop>
       <App />
     </ScrollToTop>
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );
