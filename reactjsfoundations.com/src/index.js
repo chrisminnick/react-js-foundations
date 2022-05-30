f8b562ef96ce77b566a2c66277a589f9743832a7
Chris Minnick <chris@minnick.com>
2022-05-30 06:26:56 -0700

    disabling static page generation

--- reactjsfoundations.com/src/index.js
+++ reactjsfoundations.com/src/index.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import ScrollToTop from './ScrollToTop';
 
-const container = document.getElementById('root');
-
-if (container.hasChildNodes()) {
-  hydrateRoot(
-    container,
-    <Router>
-      <ScrollToTop>
-        <App />
-      </ScrollToTop>
-    </Router>
-  );
-} else {
-  createRoot(container).render(
-    <Router>
-      <ScrollToTop>
-        <App />
-      </ScrollToTop>
-    </Router>
-  );
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
+  </Router>
+);
