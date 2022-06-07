9ee09b54d942b13bd551cb3a80a51401bd439f95
Chris Minnick <chris@minnick.com>
2022-06-07 02:47:05 -0700

    added review

--- reactjsfoundations.com/src/Introduction.js
+++ reactjsfoundations.com/src/Introduction.js
@@ -62,6 +62,23 @@ function Introduction(props) {
       </p>
       <blockquote>
         <strong>
+          "Best ReactJS book I have come across. Simply the best. This book is
+          well structured and detailed for absolute beginner to a professional.
+          I am impressed to be honest"
+        </strong>
+      </blockquote>
+      <p
+        style={{
+          textAlign: 'center',
+          color: '#b4032d',
+          fontSize: '24px',
+          marginBottom: '-10px',
+        }}
+      >
+        &#9733;&#9733;&#9733;&#9733;&#9733;
+      </p>
+      <blockquote>
+        <strong>
           "It is rare to find it hard to put down a programming book but it is
           the case for me with this one. It should be the textbook for any React
           course and for anyone learning react."
